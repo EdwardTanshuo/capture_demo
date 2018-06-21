55e78175ec7126842c449b0ed3fb4655e5542b0a
Edward Tan <etan@fulgent.com>
2018-06-21 15:55:53 -0700

    integration

---
+++ Config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define HTTP_URL "http://localhost:65000/"
+#define RTMP_URL "rtmp://127.0.0.1/live/test"
---
+++ HttpException.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+
+class HttpException : public std::exception {
+
+	std::string message;
+	int code;
+public:
+	HttpException(int err, std::string msg) : code(err), message(std::string(msg)) {};
+	~HttpException() throw() { std::cerr << "Exiting Exception" << std::endl;  };
+	std::string getMessage(void) { return message; };
+	int getCode(void) { return code; };
+};
